3494978d18218ee45680d23d998943214a2782e5
inagnone <inagnone@github.com>
2020-10-20 03:02:52 +0800

    添加eureka注册服务

---
+++ cloud_consumer_order_8010/src/main/java/org/example/icloud/controller/OrderController.java
@@ -0,0 +1,21 @@
+package org.example.icloud.controller;
+
+import org.example.icloud.common.entity.Payment;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+public class OrderController {
+    @Autowired
+    private RestTemplate restTemplate;
+    @Value("${icloud.paymeny-service-name}")
+    private String paymentServiceName;
+
+    @GetMapping("payment")
+    public void createPayment(){
+        restTemplate.postForObject("h/payment/save", Payment.class,null);
+    }
+}
---
+++ cloud_eureka_server_7001/src/test/java/com/example/icloud/IcloudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.icloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IcloudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
