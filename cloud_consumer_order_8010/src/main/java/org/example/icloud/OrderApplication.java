a7bb9927b077607094559b5ad14cadc1ea4aca51
inagnone <inagnone@github.com>
2020-11-02 01:49:08 +0800

    添加ribon自定义负载均衡规则配置

--- cloud_consumer_order_8010/src/main/java/org/example/icloud/OrderApplication.java
+++ cloud_consumer_order_8010/src/main/java/org/example/icloud/OrderApplication.java
@@ -1,11 +1,14 @@
 package org.example.icloud;
 
+import org.example.ribonRule.MyRule;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 
 @SpringBootApplication
 @EnableEurekaClient
+@RibbonClient(name = "CLOUD-PAYMENT-SERVICE",configuration = MyRule.class)
 public class OrderApplication {
 
     public static void main(String[] args) {
