5d8abe3eb0f690548782768dc5fc9862f22708ee
inagnone <inagnone@163.com>
2020-11-02 20:31:05 +0800

    添加使用feign模块

---
+++ cloud_consumner_feign_order_8011/src/main/java/org/example/icloud/OrderFeignApplication.java
@@ -0,0 +1,11 @@
+package org.example.icloud;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by 10235627@zte.intra on 20-11-2.
+ */
+@SpringBootApplication
+public class OrderFeignApplication {
+
+}
